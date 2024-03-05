1d183b2943966c78c7449210c9fcf07af29c3dc2
Thomas Martinez <thomas.martinez@igt.com>
2024-03-04 22:38:32 -0500

    updating in modal works and consoles and reaches App.js -- need to have it actually update data now

--- hello/src/App.js
+++ hello/src/App.js
@@ -8,36 +8,47 @@ function App() {
   const [role, setRole] = useState('dev');
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: 'Caleb',
       role: 'Intern',
       img: 'https://images.pexels.com/photos/2117283/pexels-photo-2117283.jpeg',
     },
     {
+      id: 2,
       name: 'Abby',
       role: 'Manager',
       img: 'https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg',
     },
     {
+      id: 3,
       name: 'John',
       role: 'Developer',
       img: 'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg',
     },
     {
+      id: 4,
       name: 'Thomas',
       role: 'Applications Engineer',
       img: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg',
     },
     {
+      id: 5,
       name: 'Chris',
       role: 'Help Desk Technician',
       img: 'https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg',
     },
     {
+      id: 6,
       name: 'Steven',
       role: 'Valet',
       img: 'https://images.pexels.com/photos/2287252/pexels-photo-2287252.jpeg',
     },
   ]);
+
+  function updateEmployee(id, newName, newRole) {
+    console.log('updateEmployee inside of App.js');
+  }
+
   const showEmployees = true;
   return (
     <div className='App'>
@@ -54,10 +65,12 @@ function App() {
             {employees.map(employee => {
               return (
                 <Employee
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
+                  updateEmployee={updateEmployee}
                 />
               );
             })}
