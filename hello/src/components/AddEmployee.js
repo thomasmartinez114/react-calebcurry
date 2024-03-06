4c991a584262fc5a67a885a8b28baeecc41403d0
Thomas Martinez <thomas.martinez@igt.com>
2024-03-05 22:04:04 -0500

    added placeholders for fullname and role input

--- hello/src/components/AddEmployee.js
+++ hello/src/components/AddEmployee.js
@@ -52,8 +52,9 @@ function AddEmployee(props) {
               </div>
               <div className='md:w-2/3'>
                 <input
-                  className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+                  className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
                   id='name'
+                  placeholder='John Smith'
                   type='text'
                   value={name}
                   onChange={e => {
@@ -73,8 +74,9 @@ function AddEmployee(props) {
               </div>
               <div className='md:w-2/3'>
                 <input
-                  className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+                  className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
                   id='role'
+                  placeholder='Bank Teller'
                   type='text'
                   value={role}
                   onChange={e => {
@@ -94,7 +96,7 @@ function AddEmployee(props) {
               </div>
               <div className='md:w-2/3'>
                 <input
-                  className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+                  className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
                   id='img'
                   placeholder='https://google.com'
                   type='text'
