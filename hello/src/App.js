86b58dbc7614c43e7e0b1e9d122486b9ec2785a5
Thomas Martinez <thomas.martinez@igt.com>
2024-03-08 21:50:17 -0500

    refactored code App.js and added Employees page

--- hello/src/App.js
+++ hello/src/App.js
@@ -6,112 +6,13 @@ import { v4 as uuidv4 } from 'uuid';
 import AddEmployee from './components/AddEmployee';
 import EditEmployee from './components/EditEmployee';
 import Header from './components/Header';
+import Employees from './pages/Employees';
 
 function App() {
-  const [role, setRole] = useState('dev');
-  const [employees, setEmployees] = useState([
-    {
-      id: 1,
-      name: 'Caleb',
-      role: 'Intern',
-      img: 'https://images.pexels.com/photos/2117283/pexels-photo-2117283.jpeg',
-    },
-    {
-      id: 2,
-      name: 'Abby',
-      role: 'Manager',
-      img: 'https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg',
-    },
-    {
-      id: 3,
-      name: 'John',
-      role: 'Developer',
-      img: 'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg',
-    },
-    {
-      id: 4,
-      name: 'Thomas',
-      role: 'Applications Engineer',
-      img: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg',
-    },
-    {
-      id: 5,
-      name: 'Chris',
-      role: 'Help Desk Technician',
-      img: 'https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg',
-    },
-    {
-      id: 6,
-      name: 'Steven',
-      role: 'Valet',
-      img: 'https://images.pexels.com/photos/2287252/pexels-photo-2287252.jpeg',
-    },
-  ]);
-
-  function newEmployee(name, role, img) {
-    const newEmployee = {
-      id: uuidv4(),
-      name: name,
-      role: role,
-      img: img,
-    };
-    setEmployees([...employees, newEmployee]);
-  }
-
-  function updateEmployee(id, newName, newRole) {
-    // console.log('updateEmployee inside of App.js');
-    const updatedEmployees = employees.map(employee => {
-      if (id === employee.id) {
-        // return new
-        return { ...employee, name: newName, role: newRole };
-      }
-
-      return employee;
-    });
-    setEmployees(updatedEmployees);
-  }
-
-  const showEmployees = true;
   return (
-    <div className='App bg-gray-300 min-h-screen'>
-      <Header />
-      {showEmployees ? (
-        <>
-          <input
-            type='text'
-            onChange={e => {
-              // console.log(e.target.value);
-              setRole(e.target.value);
-            }}
-          />
-          <div className='flex flex-wrap justify-center'>
-            {employees.map(employee => {
-              const editEmployee = (
-                <EditEmployee
-                  id={employee.id}
-                  name={employee.name}
-                  role={employee.role}
-                  updateEmployee={updateEmployee}
-                />
-              );
-              return (
-                <Employee
-                  key={employee.id}
-                  id={employee.id}
-                  name={employee.name}
-                  role={employee.role}
-                  img={employee.img}
-                  editEmployee={editEmployee}
-                />
-              );
-            })}
-          </div>
-          <AddEmployee newEmployee={newEmployee} />
-        </>
-      ) : (
-        <p>You cannot see the employees</p>
-      )}
-    </div>
+    <Header>
+      <Employees />
+    </Header>
   );
 }
 
--- hello/src/components/Header.js
+++ hello/src/components/Header.js
@@ -14,7 +14,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
-export default function Header() {
+export default function Header(props) {
   return (
     <Disclosure as='nav' className='bg-gray-800'>
       {({ open }) => (
@@ -160,6 +160,7 @@ export default function Header() {
               ))}
             </div>
           </Disclosure.Panel>
+          {props.children}
         </>
       )}
     </Disclosure>
