06161c75abfcde420488a5633e2c86fc2d7b2f34
Thomas Martinez <thomas.martinez@igt.com>
2024-03-11 10:36:45 -0400

    removed unneeded serach field

--- hello/src/App.js
+++ hello/src/App.js
@@ -18,7 +18,6 @@ function App() {
         <Routes>
           <Route path='/employees' element={<Employees />} />
           <Route path='/dictionary' element={<Dictionary />} />
-          <Route path='/definition' element={<Defintion />} />
           <Route path='/definition/:search' element={<Defintion />} />
           <Route path='/customers' element={<Customers />} />
         </Routes>
--- hello/src/pages/Dictionary.js
+++ hello/src/pages/Dictionary.js
@@ -2,19 +2,6 @@ import { useState, useEffect } from 'react';
 
 export default function Dictionary() {
   const [word, setWord] = useState('');
-  const [word2, setWord2] = useState('');
-
-  useEffect(() => {
-    console.log('State updated ' + word);
-  }, [word]);
-
-  useEffect(() => {
-    console.log('State updated ' + word2);
-  }, [word2]);
-
-  // no dependency array --> update for any state change
-  // empty dependency array --> execute once
-  // passing in data --> this will only execute when those state variables are changed
 
   return (
     <>
@@ -24,14 +11,6 @@ export default function Dictionary() {
           setWord(e.target.value);
         }}
       />
-      <h2>Let's get the definition for {word}</h2>
-      <input
-        type='text'
-        onChange={e => {
-          setWord2(e.target.value);
-        }}
-      />
-      <h2>Let's get the definition for {word2}</h2>
     </>
   );
 }
