951da73a1f588d593297a49930153cc0dfb4c6ed
Thomas Martinez <thomas.martinez@igt.com>
2024-03-11 18:47:43 -0400

    enabled enter key to submit word on search

--- hello/src/pages/Dictionary.js
+++ hello/src/pages/Dictionary.js
@@ -6,21 +6,18 @@ export default function Dictionary() {
   const navigate = useNavigate();
 
   return (
-    <>
+    <form
+      onSubmit={() => {
+        navigate('/definition/' + word);
+      }}
+    >
       <input
         type='text'
         onChange={e => {
           setWord(e.target.value);
         }}
       />
-      <button
-        onClick={() => {
-          navigate('/definition/' + word, { replace: true });
-          // console.log('click');
-        }}
-      >
-        Search
-      </button>
-    </>
+      <button>Search</button>
+    </form>
   );
 }
