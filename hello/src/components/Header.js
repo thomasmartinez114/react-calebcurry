6b37e6a5f2f1259eb8d2312478e594bbee70b6e1
Thomas Martinez <thomas.martinez@igt.com>
2024-03-11 12:29:19 -0400

    updated Employees to be state logos

--- hello/src/components/Header.js
+++ hello/src/components/Header.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
-import sphereIcon from '../media/logos/sphere-icon.png';
+import sphereIcon from '../media/images/logos/sphere-icon.png';
 import { NavLink } from 'react-router-dom';
 
 const navigation = [
