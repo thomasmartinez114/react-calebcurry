9309c22e83f89233036b725998eed7687880082a
Thomas Martinez <tmartinez114@gmail.com>
2024-03-14 22:27:03 -0400

    method 01: redirect to 404 page

--- hello/src/pages/Customer.js
+++ hello/src/pages/Customer.js
@@ -1,27 +1,39 @@
-import {useParams, Link} from 'react-router-dom';
-import {useEffect, useState} from 'react';
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-export default function Customer(){
-    const {id } = useParams();
-    const [customer, setCustomer] = useState()
-    useEffect(() => {
-        // console.log('useEffect')
-        const url = 'http://localhost:8000/api/customers/' + id
-        fetch(url).then((response) => {
-            return response.json()
-        }).then((data) => {
-            setCustomer(data.customer)
-        })
-    }, [])
+export default function Customer() {
+  const { id } = useParams();
+  const [customer, setCustomer] = useState();
+  const navigate = useNavigate();
 
-    return (
-     <>
-        {customer ? <div>
-            <p>{customer.id}</p>
-            <p>{customer.name}</p>
-            <p>{customer.industry}</p>
-            </div>: null}
-        <Link to="/customers">Go Back</Link>
-        </>
-    )
+  useEffect(() => {
+    // console.log('useEffect')
+    const url = "http://localhost:8000/api/customers/" + id;
+
+    fetch(url)
+      .then((response) => {
+        if (response.status === 404) {
+          // redirect to 404 page (new URL)
+          navigate("/404");
+          // render a 404 component in this page
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setCustomer(data.customer);
+      });
+  }, []);
+
+  return (
+    <>
+      {customer ? (
+        <div>
+          <p>{customer.id}</p>
+          <p>{customer.name}</p>
+          <p>{customer.industry}</p>
+        </div>
+      ) : null}
+      <Link to="/customers">Go Back</Link>
+    </>
+  );
 }
